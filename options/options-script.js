f2abcc299b7d0797aa512949414e89749617971e
HenryEcker <hde1997@gmail.com>
2022-05-04 17:06:32 -0400

    Logic to control options

--- options/options-script.js
+++ options/options-script.js
@@ -5,8 +5,23 @@ const d = document.getElementById("D");
 const s = document.getElementById("S");
 const m = document.getElementById("M");
 const isAdjusted = document.getElementById("adjusted");
+const bannerPattern = document.getElementById("bannerPattern");
+const enterFinalGradeD2LPattern = document.getElementById("enterFinalGradeD2LPattern");
+const editAttendanceD2LPattern = document.getElementById("editAttendanceD2LPattern");
+const attendanceRegisterCreateD2LPattern = document.getElementById("attendanceRegisterCreateD2LPattern");
+const gradeFeedbackD2LPattern = document.getElementById("gradeFeedbackD2LPattern");
+const enterZeroForMissingGrades = document.getElementById("enterZeroForMissingGrades");
+const enterZeroForMissingGradebook = document.getElementById("enterZeroForMissingGradebook");
+const bulkDateManageForAssignments = document.getElementById("bulkDateManageForAssignments");
 
-chrome.storage.sync.get(['A', 'B', 'C', 'D', 'S', 'M', 'isAdjusted'], response => {
+chrome.storage.sync.get([
+    'A', 'B', 'C', 'D', 'S', 'M',
+    'isAdjusted',
+    'bannerPattern', 'enterFinalGradeD2LPattern', 'editAttendanceD2LPattern',
+    'attendanceRegisterCreateD2LPattern', 'gradeFeedbackD2LPattern',
+    'enterZeroForMissingGrades', 'enterZeroForMissingGradebook',
+    'bulkDateManageForAssignments'
+], response => {
     a.value = response.A;
     b.value = response.B;
     c.value = response.C;
@@ -14,6 +29,14 @@ chrome.storage.sync.get(['A', 'B', 'C', 'D', 'S', 'M', 'isAdjusted'], response =
     s.value = response.S;
     m.value = response.M;
     isAdjusted.checked = response.isAdjusted;
+    bannerPattern.checked = response.bannerPattern;
+    enterFinalGradeD2LPattern.checked = response.enterFinalGradeD2LPattern;
+    editAttendanceD2LPattern.checked = response.editAttendanceD2LPattern;
+    attendanceRegisterCreateD2LPattern.checked = response.attendanceRegisterCreateD2LPattern;
+    gradeFeedbackD2LPattern.checked = response.gradeFeedbackD2LPattern;
+    enterZeroForMissingGrades.checked = response.enterZeroForMissingGrades;
+    enterZeroForMissingGradebook.checked = response.enterZeroForMissingGradebook;
+    bulkDateManageForAssignments.checked = response.bulkDateManageForAssignments;
 });
 
 const validateForm = () => {
@@ -33,7 +56,7 @@ const validateForm = () => {
 };
 
 
-document.getElementById("updateBtn").addEventListener("click", () => {
+document.getElementById("updateGradeInfoBtn").addEventListener("click", () => {
     if (validateForm()) {
         chrome.storage.sync.set({
             A: a.value,
@@ -43,6 +66,22 @@ document.getElementById("updateBtn").addEventListener("click", () => {
             S: s.value,
             M: m.value,
             isAdjusted: isAdjusted.checked
+        })
+    }
+    return true;
+});
+
+document.getElementById("updateEnabledTools").addEventListener("click", () => {
+    if (validateForm()) {
+        chrome.storage.sync.set({
+            bannerPattern: bannerPattern.checked,
+            enterFinalGradeD2LPattern: enterFinalGradeD2LPattern.checked,
+            editAttendanceD2LPattern: editAttendanceD2LPattern.checked,
+            attendanceRegisterCreateD2LPattern: attendanceRegisterCreateD2LPattern.checked,
+            gradeFeedbackD2LPattern: gradeFeedbackD2LPattern.checked,
+            enterZeroForMissingGrades: enterZeroForMissingGrades.checked,
+            enterZeroForMissingGradebook: enterZeroForMissingGradebook.checked,
+            bulkDateManageForAssignments: bulkDateManageForAssignments.checked,
         })
     }
     return true;
