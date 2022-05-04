b5e7a4f40915b4166df2f09f28fc9d91e8be19b1
HenryEcker <hde1997@gmail.com>
2022-05-04 10:49:04 -0400

    Ignore Final Adjusted Grade Column when filling with zeros

--- foreground-tasks/enter-zero-for-missing-gradebook-fg.js
+++ foreground-tasks/enter-zero-for-missing-gradebook-fg.js
@@ -1,7 +1,7 @@
 const setZeroAll = (ev) => {
     ev.preventDefault();
     $('table.d2l-table.d2l-grid.d_gd') // GradeBook Table
-        .find('d2l-input-number')//  input text field
+        .find('d2l-input-number:not([title^="Final Adjusted Grade"])')//  input text field (excludes final adjusted column)
         .each((i, e) => {
             if (!e.getAttribute('value')) {
                 e.setAttribute('value', 0);
