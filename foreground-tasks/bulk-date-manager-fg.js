ffaf2df753a45f6abfdd2ee0e289ebcd194e7d23
HenryEcker <hde1997@gmail.com>
2022-05-13 12:08:56 -0400

    Delay to bulk date manager

--- foreground-tasks/bulk-date-manager-fg.js
+++ foreground-tasks/bulk-date-manager-fg.js
@@ -1,3 +1,4 @@
+/* globals chrome, moment */
 const convertToMoment = (m) => {
     return moment(new Date(m)).utcOffset(0, true);
 };
@@ -13,11 +14,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         console.log(request);
         const startDates = [
             convertToMoment(request.startDate),
-            convertToMoment(request.dates[0]),
-            ...request.dates.slice(1).map(m => convertToMoment(m).add(1, 'days'))
+            ...request.dates.map(m => convertToMoment(m).add(1, 'days'))
         ];
         const endDates = [
-            ...request.dates.slice(1).map(m => convertToMoment(m).add(23, 'hours').add(59, 'minutes')),
+            ...request.dates.map(m => convertToMoment(m).add(23, 'hours').add(59, 'minutes')),
             convertToMoment(request.endDate).add(23, 'hours').add(59, 'minutes')
         ];
         buildSelects(startDates.map(convertToDateTime), endDates.map(convertToDateTime));
@@ -56,7 +56,7 @@ const addApplyButton = () => {
     $('d2l-button-subtle').last().closest('ul').append(li);
 };
 
-const waitForAttach = (cb) => {
+const waitForAttach = () => {
     let i = 0;
     return new Promise((resolve) => {
         $('iframe').on('load', function () {
@@ -72,7 +72,7 @@ const delayedSave = async () => {
     return new Promise((resolve) => {
         let x = new MutationObserver(function (e) {
             if (e[0].removedNodes) {
-                resolve();
+                setTimeout(resolve, 200);
             }
         });
         x.observe(document.body, {childList: true});
@@ -92,64 +92,63 @@ const changeDate = (iframe, checkboxId, dateSelectorId, new_date) => {
 
 
 const applyDates = async (ev) => {
-        ev.preventDefault();
+    ev.preventDefault();
 
-        const trs = $('#z_cl_c tbody').find('tr[role=row]');
-        let actionItems = {};
-        for (let j = 0; j < trs.length; j++) {
-            $(trs[j]).find('.my-select-dates-updater').each((i, e) => {
-                let new_date = $(e).val();
-                if (new_date) {
-                    new_date = new_date.slice(0, -1);
-                    const type = $(e).attr('dateType');
-                    let inclusion = {};
-                    if (type === '3') {
-                        inclusion['dueDate'] = new_date;
-                        inclusion['dueElem'] = e;
-                    } else if (type === '4') {
-                        inclusion['startDate'] = new_date;
-                        inclusion['accessElem'] = e;
-                    } else if (type === '5') {
-                        inclusion['endDate'] = new_date;
-                        inclusion['accessElem'] = e;
-                    }
-
-                    actionItems[j] = {
-                        ...actionItems[j],
-                        ...inclusion
-                    }
-                }
-            });
-        }
-
-        for (let [row, values] of Object.entries(actionItems)) {
-            if (values.dueDate) {
-                const b = $(values.dueElem).parent().find('div.yui-dt-liner a');
-                if (b[0]) {
-                    b[0].dispatchEvent(new Event("click"));
-                    const iframe = await waitForAttach();
-                    changeDate(iframe, '#z_j', '#z_k', values.dueDate);
+    const trs = $('#z_cl_c tbody').find('tr[role=row]');
+    let actionItems = {};
+    for (let j = 0; j < trs.length; j++) {
+        $(trs[j]).find('.my-select-dates-updater').each((i, e) => {
+            let new_date = $(e).val();
+            if (new_date) {
+                new_date = new_date.slice(0, -1);
+                const type = $(e).attr('dateType');
+                let inclusion = {};
+                if (type === '3') {
+                    inclusion['dueDate'] = new_date;
+                    inclusion['dueElem'] = e;
+                } else if (type === '4') {
+                    inclusion['startDate'] = new_date;
+                    inclusion['accessElem'] = e;
+                } else if (type === '5') {
+                    inclusion['endDate'] = new_date;
+                    inclusion['accessElem'] = e;
                 }
 
-                await delayedSave();
+                actionItems[j] = {
+                    ...actionItems[j],
+                    ...inclusion
+                }
             }
-            if (values.endDate || values.startDate) {
-                const b = $(values.accessElem).parent().find('div.yui-dt-liner a');
-                if (b[0]) {
-                    b[0].dispatchEvent(new Event("click"));
-                    const iframe = await waitForAttach();
-                    if (values.endDate) {
-                        changeDate(iframe, '#z_q', '#z_r', values.endDate);
-                    }
-                    if (values.startDate) {
-                        changeDate(iframe, '#z_n', '#z_o', values.startDate);
-                    }
-                }
-
-                await delayedSave();
-            }
-        }
-
-        $('.my-select-dates-updater').val('');
+        });
     }
-;
+
+    for (let [row, values] of Object.entries(actionItems)) {
+        if (values.dueDate) {
+            const b = $(values.dueElem).parent().find('div.yui-dt-liner a');
+            if (b[0]) {
+                b[0].dispatchEvent(new Event("click"));
+                const iframe = await waitForAttach();
+                changeDate(iframe, '#z_j', '#z_k', values.dueDate);
+            }
+
+            await delayedSave();
+        }
+        if (values.endDate || values.startDate) {
+            const b = $(values.accessElem).parent().find('div.yui-dt-liner a');
+            if (b[0]) {
+                b[0].dispatchEvent(new Event("click"));
+                const iframe = await waitForAttach();
+                if (values.endDate) {
+                    changeDate(iframe, '#z_q', '#z_r', values.endDate);
+                }
+                if (values.startDate) {
+                    changeDate(iframe, '#z_n', '#z_o', values.startDate);
+                }
+            }
+
+            await delayedSave();
+        }
+    }
+
+    $('.my-select-dates-updater').val('');
+};
